94c15018477a2665a3c59b17087858b0317658c7
Frandes Eduard <edy_21_2001@yahoo.com>
2023-10-10 03:20:53 +0300

    travelAg

--- src/main/java/org/fasttrack/ui/OfferUIController.java
+++ src/main/java/org/fasttrack/ui/OfferUIController.java
@@ -5,6 +5,7 @@ import org.fasttrack.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class OfferUIController {
     public OfferUIController(OfferService service) {
         this.service = service;
     }
-
+    @GetMapping("offers-app")
     String getOffers(Model model) {
         List<Offer> offersFromDB = service.getAllOffers();
         model.addAttribute("offers", offersFromDB);
